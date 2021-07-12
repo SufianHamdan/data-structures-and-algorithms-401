ff2e9f1fb4975a61a263827f63b319a1db9a414a
SufianHamdan <sufian_hamdan@yahoo.com>
2021-07-12 23:28:40 +0300

    not complete

---
+++ Stack-Queue/app/src/main/java/stack/Queue/Animal.java
@@ -0,0 +1,13 @@
+package stack.Queue;
+
+public class Animal<T> {
+    private String type = "";
+
+    public Animal(String type){
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
